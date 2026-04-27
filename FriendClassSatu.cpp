0ab9b5d10ac565b0782cb3fe274c43853f2b3acd
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-04-27 15:50:48 +0800

    implementaasi setname dan setId

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -27,3 +27,12 @@ void siswa::displayAll(orang &a)
 {
     cout << id << endl << a.nama;
 }
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+
