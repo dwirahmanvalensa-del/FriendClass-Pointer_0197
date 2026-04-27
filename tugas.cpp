94196c3274af6146513e422cd833c89439dee084
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-04-27 20:11:26 +0800

    menambahkan fungsi hitungluas pada LayangLayang

--- tugas.cpp
+++ tugas.cpp
@@ -23,3 +23,35 @@ public:
         cin >> d2;
     }
 
+    double hitungLuas()
+    {
+        return 0.5 * d1 * d2;
+    }
+
+    friend void tampilkanKeliling(LayangLayang, BelahKetupat);
+    friend class BelahKetupat;
+};
+
+class BelahKetupat
+{
+private:
+    double s, d1, d2;
+
+public:
+    void inputData()
+    {
+        cout << "\nInput Belah Ketupat" << endl;
+        cout << "Masukkan sisi 1 : ";
+        cin >> s;
+        cout << "Masukkan diagonal 1 : ";
+        cin >> d1;
+        cout << "Masukkan diagonal 2 : ";
+        cin >> d2;
+    }
+
+    double hitungLuas()
+    {
+        return 0.5 * d1 * d2;
+    }
+
+    
